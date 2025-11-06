a3e56420fd87491a7eef32c35ea38b7565287be5
mro <maro10@protonmail.com>
2025-11-06 19:22:10 +0100

    ci fix

--- farmerjoe/next.config.ts
+++ farmerjoe/next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  // Disable telemetry
-  telemetry: false,
 };
 
 export default nextConfig;
